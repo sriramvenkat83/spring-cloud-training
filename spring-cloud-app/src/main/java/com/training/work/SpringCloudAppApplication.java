a335abc9244055f53c596d2b9bf7c881ac4d42c6
Sriram Venkataraman <svenkataraman@nisum.com>
2017-01-08 16:40:48 -0800

    added spring-cloud-app and spring-cloud-config-server app

---
+++ spring-cloud-app/src/main/java/com/training/work/SpringCloudAppApplication.java
@@ -0,0 +1,12 @@
+package com.training.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudAppApplication.class, args);
+	}
+}
